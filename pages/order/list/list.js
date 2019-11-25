1ae202b62c5225e23c5d7cf0d8ec87abf716d657
syf <15062286502@163.com>
2019-11-25 22:45:20 +0800

    '11.25'

--- pages/order/list/list.js
+++ pages/order/list/list.js
@@ -6,33 +6,15 @@ Page({
    * 页面的初始数据
    */
   data: {
-    tabIndex:0
+    tabIndex:0,
+    takeInList:[],
+    takeOutList: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    wx.request({
-      url: 'http://localhost:8081/vx/takeInOrder',
-      method: 'GET',
-      data: {
-        "openId": wx.getStorageSync('openId')
-      },
-      header: {
-        'Accept': 'application/json'
-      },
-      success: function (res) {
-        console.log(res.data)
-      
-      },
-      fail: function (res) {
-        wx.showToast({
-          title: '服务器异常',
-          icon: 'none'
-        })
-      }
-    })
   },
 
   changeTab:function(e){
@@ -46,4 +28,30 @@ Page({
       url: '../../list/list'
     })
   },
+  onShow: function(){
+    var that =this;
+    wx.request({
+      url: 'http://localhost:8081/vx/takeInOrder',
+      method: 'GET',
+      data: {
+        "openId": wx.getStorageSync('openId')
+      },
+      header: {
+        'Accept': 'application/json'
+      },
+      success: function (res) {
+        console.log(res.data)
+        that.setData({
+          takeInList: res.data
+        })
+
+      },
+      fail: function (res) {
+        wx.showToast({
+          title: '服务器异常',
+          icon: 'none'
+        })
+      }
+    })
+  }
 })
